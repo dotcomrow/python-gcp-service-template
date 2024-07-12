f58bb6672dbec9d51c8878600bcfbaf92a22115f
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-07-12 20:17:39 +0000

    Deploying to release from @ dotcomrow/python-gcp-service-template@979d524fad009ba0b402ca67f9aff393b1a77a4f 🚀

---
+++ src/run_locally.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# AUDIENCE = os.getenv('AUDIENCE')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+# DATA_LAYER_URL = os.getenv('PREFERENCES_DATA_LAYER_URL')
+# PROJECT_ID = os.getenv('PROJECT_ID')
+# CONTEXT_ROOT = os.getenv('CONTEXT_ROOT')
+# REGION = os.getenv('REGION')
+# K_SERVICE = os.getenv('K_SERVICE')
+# DOMAIN = os.getenv('DOMAIN')
+
+docker build -t data-svc:latest .
+docker run --rm -p 8080:8080 -e PORT=8080 \
+    -e PROJECT_ID=configuration-20231112 \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/secrets/google.key \
+    -e CONTEXT_ROOT=user-profile-ol-svc \
+    -e DOMAIN=suncoast.systems \
+    -v /Users/admin/secrets:/secrets \
+      data-svc:latest
